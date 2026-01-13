723e5da733041d007a11049050a54c4fb5817e67
Fernandes Lucas <lucas.fernandes3319@gmail.com>
2026-01-13 15:43:02 +0100

    Commit I.a - Utilisation du timer 2 Commentaire a jour

--- TP1c_timer2.X/main.c
+++ TP1c_timer2.X/main.c
@@ -4,16 +4,16 @@
 #define _XTAL_FREQ 8000000
 
 // Définitions des masques de LED
-#define MASK_LED1 0X01
-#define MASK_LED2 0X02
-#define MASK_LED3 0X04
-#define MASK_LED4 0X08
-#define MASK_LED5 0X01
-#define MASK_LED6 0X02
-#define MASK_LED7 0X04
-#define MASK_LED8 0X08
+#define MASK_LED1 0X01 //LED1
+#define MASK_LED2 0X02 //LED2
+#define MASK_LED3 0X04 //LED3
+#define MASK_LED4 0X08 //LED4
+#define MASK_LED5 0X01 //LED5
+#define MASK_LED6 0X02 //LED6
+#define MASK_LED7 0X04 //LED7
+#define MASK_LED8 0X08 //LED8
 
-/* Configuration du timer2 avec une période de 100 ms */
+/* Configuration du timer2 avec une période de 100 us */
 void config_timer2(void){
 PR2 = 24; 
 T2CONbits.T2CKPS = 1; // Prescaler = 1:4
@@ -36,52 +36,41 @@ void wait_125ms(void){
 
 void main(void) {
    
-    LATD = 0x00;
-    LATB = 0x00;
+    LATD = 0x00;// Mise à zéro des LED1-4
+    LATB = 0x00;// Mise à zéro des LED5-8
     
-    config_timer2();
+    config_timer2();// Appel de la fonction "config_timer2()"
     
-    TRISD &= MASK_LED1;
-    TRISD &= MASK_LED2;
-    TRISD &= MASK_LED3;
-    TRISD &= MASK_LED4;
-    TRISB &= MASK_LED5;
-    TRISB &= MASK_LED6;
-    TRISB &= MASK_LED7;
-    TRISB &= MASK_LED8;
-    
-    
+    TRISD = 0x00; // Tout le port D en sortie 
+    TRISB = 0x00; // Tout le port B en sortie 
 
     while(1){ 
         
-        LATD = 0x00;
-        LATB = 0x00;
         
         LATD |= MASK_LED1;
-        wait_125ms();
-        LATD = 0x00;
+        wait_125ms();//Attente
+        LATD = 0x00;// Mise à zéro des LED1-4
         LATD |= MASK_LED2;
-        wait_125ms();
-        LATD = 0x00;
+        wait_125ms(); //Attente
+        LATD = 0x00;// Mise à zéro des LED1-4
         LATD |= MASK_LED3;
-        wait_125ms();
-        LATD = 0x00;
+        wait_125ms();//Attente
+        LATD = 0x00;// Mise à zéro des LED1-4
         LATD |= MASK_LED4;
-        wait_125ms();
-        LATD = 0x00;
+        wait_125ms();//Attente
+        LATD = 0x00;// Mise à zéro des LED1-4
         LATB |= MASK_LED5;
-        wait_125ms();
-        LATB = 0x00;
+        wait_125ms();//Attente
+        LATB = 0x00;// Mise à zéro des LED5-8
         LATB |= MASK_LED6;
-        wait_125ms();
-        LATB = 0x00;
+        wait_125ms();//Attente
+        LATB = 0x00;// Mise à zéro des LED5-8
         LATB |= MASK_LED7;
-        wait_125ms();
-        LATB = 0x00;
+        wait_125ms();//Attente
+        LATB = 0x00;// Mise à zéro des LED5-8
         LATB |= MASK_LED8;
-        wait_125ms();
-        LATB = 0x00
-                ;
+        wait_125ms();//Attente
+        LATB = 0x00;// Mise à zéro des LED5-8
        
     }
 }
