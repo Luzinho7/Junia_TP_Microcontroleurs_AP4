9af56e92bc6a859d8b9e95a50a40792a909804b0
Fernandes Lucas <lucas.fernandes3319@gmail.com>
2026-02-10 13:47:44 +0100

    Premier commit TP César

---
+++ TP3d_cesar.X/main.c
@@ -0,0 +1,12 @@
+/*
+ * Projet : TP3d_cesar
+ * Auteur : Etudiant
+ * Description : Tout le code (LCD, SPI, UART, ADC, Cesar) dans un seul fichier.
+ */
+
+#include "configbits.h" // Assurez-vous que ce fichier existe, sinon configurez les bits ici
+#include <xc.h>
+#include <string.h>
+
+#define _XTAL_FREQ 8000000 // Quartz 8MHz
+
